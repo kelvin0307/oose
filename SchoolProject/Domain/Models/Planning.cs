16ac3930261f482f2daca3159f7240f9f526977b
kelvin0307 <kelvinlvb2002@hotmail.com>
2025-12-13 20:14:54 +0100

    Created necessary models
    I Created the necessary models without relations

---
+++ SchoolProject/Domain/Models/Planning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Planning
+    {
+        public int Id { get; set; }
+        public int WeekNumber { get; set; }
+        public string Name { get; set; }
+        public int SequenceNumber { get; set; }
+    }
+}
---
+++ SchoolProject/Domain/Models/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Test : Lesson
+    {
+        public int Weight { get; set; }
+        public TestType TestType { get; set; }
+    }
+}
+
+
+public enum TestType
+{
+
+}
