90693d69c796457f36d0b7ce92d62bb4b407c0bc
Casper Bruning <82331679+CasperBruning@users.noreply.github.com>
2025-12-22 22:29:16 +0100

    add local dbconnection

--- Domain/Models/Course.cs
+++ Domain/Models/Course.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
 
-        public Planning Planning { get; set; }
+        public Planning? Planning { get; set; }
         public ICollection<LearningOutcome> LearningOutcomes { get; set; } = new List<LearningOutcome>();
     }
 }
