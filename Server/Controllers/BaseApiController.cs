2c98c2d14ba489949806a468d305aee57932e670
Casper Bruning <82331679+CasperBruning@users.noreply.github.com>
2025-12-22 22:05:06 +0100

    Created BaseApiController

---
+++ Server/Controllers/BaseApiController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Core.Common;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected IActionResult HandleResponse<T>(Response<T> response, bool noContentOnSuccess = false)
+        {
+            if (response.Success)
+                return noContentOnSuccess ? NoContent() : Ok(response.Result);
+
+            return response.Status switch
+            {
+                ResponseStatus.NotFound => NotFound(new { message = response.Message }),
+                ResponseStatus.ValidationError => BadRequest(response.ValidationErrors),
+                ResponseStatus.InvalidOperation => BadRequest(new { message = response.Message }),
+                ResponseStatus.Unauthorized => Unauthorized(new { message = response.Message }),
+                ResponseStatus.Forbidden => Forbid(),
+                _ => StatusCode(500, new { message = response.Message })
+            };
+        }
+
+        protected IActionResult HandleCreatedResponse<T>(Response<T> response, string actionName, object routeValues)
+        {
+            if (!response.Success)
+                return HandleResponse(response);
+
+            return CreatedAtAction(actionName, routeValues, response.Result);
+        }
+    }
+}
