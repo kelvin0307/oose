665a73342914b88a260c761e1025e68b96c905b0
TimVerbeekAtHAN <t.verbeek1@student.han.nl>
2025-12-30 18:57:20 +0100

    Made changes as requested, Added extentionmethods for ToDTO and ToModel

--- Core/Mappers/MappingProfile.cs
+++ Core/Mappers/MappingProfile.cs
@@ -2,13 +2,12 @@
 using Core.DTOs;
 using Domain.Models;
 
-namespace Core.Mappers
+namespace Core.Mappers;
+public class MappingProfile : Profile
 {
-    public class MappingProfile : Profile
+    public MappingProfile()
     {
-        public MappingProfile()
-        {
-            CreateMap<Course, CourseDto>().ReverseMap();
-        }
+        CreateMap<Course, CourseDto>().ReverseMap();
+        CreateMap<CreateCourseDto, Course>();
     }
 }
