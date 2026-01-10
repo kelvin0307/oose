e530b457fdf531279df721dd6c7116a799ad244c
Casper Bruning <82331679+CasperBruning@users.noreply.github.com>
2026-01-10 13:23:33 +0000

    Moved services to core extension

--- Core/Extentions/CoreExtensions.cs
+++ Core/Extentions/CoreExtensions.cs
@@ -17,6 +17,7 @@ public static class CoreExtensions
         QuestPDF.Settings.License = LicenseType.Community;
 
         services.AddTransient<ICourseService, CourseService>();
+        services.AddTransient<ILearningOutcomeService, LearningOutcomeService>();
         services.AddTransient<IValidatorService, ValidatorService>();
         services.AddTransient<IPlanningService, PlanningService>();
         services.AddTransient<IDocumentFactory, DocumentFactory>();
